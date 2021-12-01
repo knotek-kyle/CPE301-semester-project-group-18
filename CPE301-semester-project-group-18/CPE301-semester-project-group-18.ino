1279ab916c3ba6d56afaa46d94e7f989d0270a9a
knotek-kyle <70342120+knotek-kyle@users.noreply.github.com>
2021-12-01 12:22:48 -0800

    new functions

--- CPE301-semester-project-group-18/CPE301-semester-project-group-18.ino
+++ CPE301-semester-project-group-18/CPE301-semester-project-group-18.ino
@@ -1,9 +1,13 @@
 //Kyle Knotek, Clifford Stewart, Joel Castillo
+#include <LiquidCrystal.h>
+#include "dht.h"
 
 #define DHT11_PIN 54
 #define WS_ADC_ID 1
 #define WATER_MIN 64
 #define ANALOGUE_PIN( n ) ( 54 + n )
+
+void adc_init();
 
 volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
@@ -29,4 +33,12 @@ void loop()
   // Delay for 2 seconds
   delay(2000);
 
+}
+
+void adc_init()
+{
+  *my_ADCSRA |= 0b10000000;
+  *my_ADCSRA &= 0b11010000;
+  *my_ADCSRB &= 0b11110000;
+  *my_ADMUX = 0b10000000;
 }
